8e98d5cb945f6f308367a7b059046619b2dac5b3
WillyTALOM <103586241+WillyTALOM@users.noreply.github.com>
2023-02-13 17:27:02 +0100

    contactcontroller done

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact')]
+    public function index(Request $request, SluggerInterface $slugger, MailerInterface $mailer): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+         $contact = $form->getData();
+         $email = (new TemplatedEmail())
+              ->from(new Address($contact["email"], $contact["firstName"] . '' . $contact["lastName"])) 
+              ->to(new Address($this->getParameter('contact_email')))
+              ->replyTo(new Address($contact['email'], $contact['firstName']. '' . $contact['lastName']))
+              ->subject('Demande de contact - ' . $contact['subject'])
+              ->htmlTemplate('contact/email_contact.html.twig')
+              ->context([
+                'firstName' => htmlspecialchars($contact['firstName']),
+                'lastName' => htmlspecialchars($contact['lastName']),
+                'company' => htmlspecialchars($contact['company']),
+                'phone' => htmlspecialchars($contact['phone']),
+                'emailAddress' => htmlspecialchars($contact['email']),
+                'subject' => htmlspecialchars($contact['subject']),
+                'message' => htmlspecialchars($contact['message']),
+                           
+              ]); 
+              
+              if ($contact['attachment'] !== null) {
+                $originalFileName = pathinfo($contact['attachment']->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFileName = $slugger->slug($originalFileName);
+                $newFileName = $safeFileName . '.' . $contact['attachment']->guessExtension();
+                $email->attachFromPath($contact['attachment']->getPathName(), $newFileName);
+            }
+
+            $mailer->send($email);
+            $this->addFlash('success', 'Votre message a bien été envoyé. Merci de nous avoir contacté. Notre équipe va vous répondre dans les meilleurs délais.');
+            return $this->redirectToRoute('contact');
+            
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'contactForm' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/ContactType.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('firstName', TextType::class, [
+            'attr' => [
+                'maxLength' => 100
+            ]
+        ])
+        ->add('lastName', TextType::class, [
+            'attr' => [
+                'maxLength' => 100
+            ]
+        ])
+        ->add('phone', NumberType::class, [
+            'attr' => [
+                'maxLength' => 100
+            ]
+        ])
+        ->add('company', TextType::class, [
+            'required' => false,
+            'attr' => [
+                'maxLenght' => 100
+            ]
+        ])
+        ->add('email', EmailType::class, [
+            'attr' => [
+                'maxLenght' => 100
+            ]
+        ])
+
+        ->add('subject', ChoiceType::class, [
+            'choices' => [
+                '---choix---' => '',
+                'commande' => 'commande',
+                'livraison' => 'livraison',
+                'Votre compte' => 'compte',
+                'signaler un bug' => 'problem',
+                'SAV' => 'SAV',
+                'autre' => 'autre'
+            ]
+        ])
+
+        ->add('message', TextareaType::class, [
+            'attr' => [
+                'minLenght' => 50,
+                'maxLenght' => 3000
+            ],
+            'help' => '3000 caractères maximum'
+        ])
+
+        ->add('attachment', FileType::class, [
+            'required' => false,
+            'help' => 'image ou document PDF',
+            'constraints' => [
+                new File([
+                    'maxSize' => '2M',
+                    'maxSizeMessage' => 'Le fichier est trop volumineux ({{ size }}) ({{ suffix }}). La taille maximale autorisée est de ({{ limit }}) ({{ suffix }}) .',
+                    'mimeTypes' => [
+                        'image/*',
+                        'application/pdf'
+                    ],
+                    'mimeTypesMessage' => 'le type de fichier est invalide ({{ type}}). Les types autorisés sont : ({{ type }})'
+
+
+                ])
+            ]
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
