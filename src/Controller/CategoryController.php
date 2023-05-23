1c41656eaf3fb418adf8683b28b2ea29eaad4594
WillyTALOM <103586241+WillyTALOM@users.noreply.github.com>
2023-05-23 14:04:51 +0200

    Update CategoryController.php
    minor changes in category controller

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -23,9 +23,29 @@ class CategoryController extends AbstractController
         //     $request->query->getInt('page', 1),
         //     12
         // );
-
+        $categorySlug = $categoryRepository->findOneBy(['slug'=> $slug]);
         return $this->render('partials/header.html.twig', [
             // 'products' => $products,
+            'categories' => $categories
+        ]);
+    }
+    
+    #[Route('/products/{category}', name: 'products_by_category')]
+    public function getProduitsByCategory(string $category, CategoryRepository $categoryRepository, ProductRepository $productRepository, Request $request): Response
+    {
+        $category = $categoryRepository->findOneBy(['name' => $category]);
+        $categories = $categoryRepository->findAll();
+        // $data = $products;
+
+        // $products = $paginator->paginate(
+        //     $data,
+        //     $request->query->getInt('page', 1),
+        //     12
+        // );
+
+        return $this->render('partials/header.html.twig', [
+            'products' => $productRepository->findBy(['category' => $category]),
+            'category' => $category,
             'categories' => $categories
         ]);
     }
