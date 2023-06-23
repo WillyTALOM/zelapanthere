5f3bfb7bb0f094e6185b79ecde54a15f4bde70fe
WillyTALOM <103586241+WillyTALOM@users.noreply.github.com>
2023-06-23 16:15:03 +0200

    hebergement mais site non fini

--- src/Controller/PaypalPaymentController.php
+++ src/Controller/PaypalPaymentController.php
@@ -15,91 +15,152 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PaypalPaymentController extends AbstractController
 {
-    #[Route('/payment-paypal/{order}', name: 'payment_paypal')]
-    public function paypal(Request $request, CartService $cartService, Order $order,ManagerRegistry $managerRegistry, UrlGeneratorInterface $urlGeneratorInterface): RedirectResponse{
+    #[Route('/payment-paypal', name: 'payment_paypal')]
+    public function ui(CartService $cart): string
+    {
+     
         
-        $manager = $managerRegistry->getManager();
-        $sessionCart = $cartService->getCart(); // récupère le panier en session
-        $items = []; // initialise le panier paypal (qui sera envoyé à paypal)
-        $itemTotal = 0;
-        foreach ($sessionCart as $line) { // transforme le panier session en panier paypal
-            if ($line['product']->getReduction() > 0) {
-                $items = [ // produit tel que Stripe en a besoin pour le traiter, les noms des index sont importants
-                    'name' => $line['product']->getName(),
-                    'quantity' => $line['quantity'],
-                    'unit_amount' => [
-                        'currency_code' => 'EUR',
-                        'value' => $line['product']->getPriceSold() * 100,
-                    ]
-                ];
-            $itemTotal += $line['quantity'] * $line['product']->getPriceSold();
-            
-        } 
-        $carrier = $order->getCarrier();
-        $total = $itemTotal + $carrier->getPrice();
-
-        $request = new OrdersCreateRequest();
-        // $request->prefer('return-representation');
-
-        $request->body = [
-            'intent' => 'CAPTURE',
+        $clientSecret = 'EEUfWThCSBIzQigDSHKjL31TjK5a6myRrfLm0eq1PYFJ2q4ACyjBVZs1nfg0yS0W8YBBO6gWJTufo-fJ';
+         
+        
+        $clientId = 'Afiy80K595O5adN8b0etICGx0UZz0Bw1uGwMmPJ55dXovRaAAbEMO9SumubGfWR3lnQp-6nSoKoW1fks';
+        $order = json_encode([
             'purchase_units' => [
                 [
-                    'amount' => [
+                    'description' => 'Panier',
+                    'items'       => array_map(function ($product) {
+                        return [
+                            'name'        => $product['name'], 
+                            'quantity'    => 1,
+                            'unit_amount' => [
+                                'value'         => number_format($product['price'] / 100, 2, '.', ""), // Mes sommes sont en centimes d'euros
+                                'currency_code' => 'EUR',
+                            ]
+                        ];
+                    }, $cart->getCart()),
+                    'amount'      => [
                         'currency_code' => 'EUR',
-                        'value' => $total,
-                        'breakdown' => [
+                        'value'         => number_format($cart->getTotal() / 100, 2, '.', ""),
+                        'breakdown'     => [
                             'item_total' => [
                                 'currency_code' => 'EUR',
-                                'value' => $itemTotal
-                            ],
-                            'shipping' => [
-                                'currency_code' => 'EUR',
-                                'value' => $carrier->getPrice()
+                                'value'         => number_format($cart->getTotal() / 100, 2, '.', "")
                             ]
                         ]
-                    ],
-                    'items' => $items
+                    ]
                 ]
-                ],
-                'application_context' => [
-                    'return_url' => $urlGeneratorInterface->generate(
-                        'payment_success_paypal',
-                        ['order' => $order->getId()],
-                        UrlGeneratorInterface::ABSOLUTE_URL 
-                    ),
-                    'cancel_url' => $urlGeneratorInterface->generate(
-                        'payment_cancel_paypal',
-                        ['order' => $order->getId()], 
-                        UrlGeneratorInterface::ABSOLUTE_URL 
-                    )
-                ]
-            ];
-
-        }
-        $client = $this->getClientPaypal();
-        $response = $client->execute($request);
-        if($response->statusCode !== 201){
-            return $this->redirectToRoute('Cart');
-        }
-
-        $approvalLink = '';
-        foreach($response->result->links as $link){
-            if($link->rel === 'approve'){
-               $approvalLink = $link->href;
-               break;
+            ]
+        ]);
+        return <<<HTML
+        <script src="https://www.paypal.com/sdk/js?client-id={$clientId}&currency=EUR&intent=authorize"></script>
+        <div id="paypal-button-container"></div>
+        <script>
+          paypal.Buttons({
+            // Sets up the transaction when a payment button is clicked
+            createOrder: (data, actions) => {
+              return actions.order.create({$order});
+            },
+            // Finalize the transaction after payer approval
+            onApprove: async (data, actions) => {
+              const authorization = await actions.order.authorize()
+              const authorizationId = authorization.purchase_units[0].payments.authorizations[0].id
+              await fetch('/paypal.php', {
+                method: 'post',
+                headers: {
+                  'content-type': 'application/json'
+                },
+                body: JSON.stringify({authorizationId})
+              })
+              alert('Votre paiement a bien été enregistré')
             }
-        }
-        if(empty($approvalLink)){
-            return $this->redirectToRoute('Cart');  
-        }
-        $order->setPaypal($response->result->id);
-
-        $manager->persist($order);
-        $manager->flush();
-
-        return new RedirectResponse($approvalLink);
+          }).render('#paypal-button-container');
+        </script>
+    HTML;
     }
+    // public function paypal(Request $request, CartService $cartService, Order $order,ManagerRegistry $managerRegistry, UrlGeneratorInterface $urlGeneratorInterface): RedirectResponse{
+        
+    //     $manager = $managerRegistry->getManager();
+    //     $sessionCart = $cartService->getCart(); // récupère le panier en session
+    //     $items = []; // initialise le panier paypal (qui sera envoyé à paypal)
+    //     $itemTotal = 0;
+    //     foreach ($sessionCart as $line) { // transforme le panier session en panier paypal
+    //         if ($line['product']->getReduction() > 0) {
+    //             $items = [ // produit tel que Stripe en a besoin pour le traiter, les noms des index sont importants
+    //                 'name' => $line['product']->getName(),
+    //                 'quantity' => $line['quantity'],
+    //                 'unit_amount' => [
+    //                     'currency_code' => 'EUR',
+    //                     'value' => $line['product']->getPriceSold() * 100,
+    //                 ]
+    //             ];
+    //         $itemTotal += $line['quantity'] * $line['product']->getPriceSold();
+            
+    //     } 
+    //     $carrier = $order->getCarrier();
+    //     $total = $itemTotal + $carrier->getPrice();
+
+    //     $request = new OrdersCreateRequest();
+    //     // $request->prefer('return-representation');
+
+    //     $request->body = [
+    //         'intent' => 'CAPTURE',
+    //         'purchase_units' => [
+    //             [
+    //                 'amount' => [
+    //                     'currency_code' => 'EUR',
+    //                     'value' => $total,
+    //                     'breakdown' => [
+    //                         'item_total' => [
+    //                             'currency_code' => 'EUR',
+    //                             'value' => $itemTotal
+    //                         ],
+    //                         'shipping' => [
+    //                             'currency_code' => 'EUR',
+    //                             'value' => $carrier->getPrice()
+    //                         ]
+    //                     ]
+    //                 ],
+    //                 'items' => $items
+    //             ]
+    //             ],
+    //             'application_context' => [
+    //                 'return_url' => $urlGeneratorInterface->generate(
+    //                     'payment_success_paypal',
+    //                     ['order' => $order->getId()],
+    //                     UrlGeneratorInterface::ABSOLUTE_URL 
+    //                 ),
+    //                 'cancel_url' => $urlGeneratorInterface->generate(
+    //                     'payment_cancel_paypal',
+    //                     ['order' => $order->getId()], 
+    //                     UrlGeneratorInterface::ABSOLUTE_URL 
+    //                 )
+    //             ]
+    //         ];
+
+    //     }
+    //     $client = $this->getClientPaypal();
+    //     $response = $client->execute($request);
+    //     if($response->statusCode !== 201){
+    //         return $this->redirectToRoute('Cart');
+    //     }
+
+    //     $approvalLink = '';
+    //     foreach($response->result->links as $link){
+    //         if($link->rel === 'approve'){
+    //            $approvalLink = $link->href;
+    //            break;
+    //         }
+    //     }
+    //     if(empty($approvalLink)){
+    //         return $this->redirectToRoute('Cart');  
+    //     }
+    //     $order->setPaypal($response->result->id);
+
+    //     $manager->persist($order);
+    //     $manager->flush();
+
+    //     return new RedirectResponse($approvalLink);
+    // }
     
     #[Route('/payment/{order}/success-paypal', name: 'payment_success_paypal')]
     public function successPaypal(Order $order, CartService $cartService, OrderStateRepository $orderStateRepository)
