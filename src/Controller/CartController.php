70c35754488e4ce0565e719c35c744c9187dd937
WillyTALOM <103586241+WillyTALOM@users.noreply.github.com>
2023-06-28 00:58:30 +0200

    Update CartController.php

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -127,9 +127,14 @@ class CartController extends AbstractController
 
                 // traite le transporteur comme un produit (± ajout au panier)
                 // if($request->request->get('stripe')){
+                if ($cartValidationForm['payment']->getData() === 'stripe'){
                     return $this->redirectToRoute('payment_stripe', [
-                        'order' => $order->getId()
-                    ]);
+                        'orde' => $order->getId()
+                    ]);}
+                    else{return $this->redirectToRoute('payment_paypal', [
+                        'orde' => $order->getId()
+                    ]);}
+                        
                 // }
                 // if($request->request->get('paypal')){
                 //     return $this->redirectToRoute('paymentpaypal', [
