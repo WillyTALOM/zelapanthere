a952451de41b45c7b5a814bdaaf272e77d0d6602
WillyTALOM <103586241+WillyTALOM@users.noreply.github.com>
2023-06-29 18:02:05 +0200

    Update CategoryController.php

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,6 +7,7 @@ use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class CategoryController extends AbstractController
 {
