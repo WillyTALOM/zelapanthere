d3bda5a0e50e22251ca88d3356c13dbd39b19c04
WillyTALOM <103586241+WillyTALOM@users.noreply.github.com>
2023-07-01 00:34:38 +0200

    Update ReceiptController.php

--- src/Controller/ReceiptController.php
+++ src/Controller/ReceiptController.php
@@ -31,6 +31,7 @@ class ReceiptController extends AbstractController
             'product' => $product,
             'order' => $order,
             'user' => $this->getUser(),
+            'logo'  => $this->imageToBase64($this->getParameter('kernel.project_dir') . '/public/img/profile.png'),
             // 'orderDetails' => $orderDetails
 
         ]);
@@ -48,5 +49,32 @@ class ReceiptController extends AbstractController
             // 'orderDetails' => $orderDetails
 
         ]);
+    }
+    
+          $data = [
+            'imageSrc'  => $this->imageToBase64($this->getParameter('kernel.project_dir') . '/public/img/profile.png'),
+            'name'         => 'John Doe',
+            'address'      => 'USA',
+            'mobileNumber' => '000000000',
+            'email'        => 'john.doe@email.com'
+        ];
+        $html =  $this->renderView('pdf_generator/index.html.twig', $data);
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml($html);
+        $dompdf->render();
+         
+        return new Response (
+            $dompdf->stream('resume', ["Attachment" => false]),
+            Response::HTTP_OK,
+            ['Content-Type' => 'application/pdf']
+        );
+    }
+ 
+    private function imageToBase64($path) {
+        $path = $path;
+        $type = pathinfo($path, PATHINFO_EXTENSION);
+        $data = file_get_contents($path);
+        $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+        return $base64;
     }
 }
