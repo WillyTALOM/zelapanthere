acdb01d321b2a4f1b794c83160dd802240bd7795
WillyTALOM <103586241+WillyTALOM@users.noreply.github.com>
2023-06-30 19:14:53 +0200

    Update Address.php

--- src/Entity/Address.php
+++ src/Entity/Address.php
@@ -26,12 +26,6 @@ class Address
 
     #[ORM\Column(length: 50)]
     private ?string $city = null;
-
-    #[ORM\Column(length: 20)]
-    private ?string $firstName = null;
-    
-    #[ORM\Column(length: 20)]
-    private ?string $lastName = null;
     
     #[ORM\Column(length: 20)]
     private ?string $country = null;
@@ -161,28 +155,5 @@ class Address
 
         return $this;
     }
-    
-    public function getFirstName(): ?string
-    {
-        return $this->firstName;
-    }
 
-    public function setFirstName(string $firstName): self
-    {
-        $this->firstName = $firstName;
-
-        return $this;
-    }
-    
-    public function getLastName(): ?string
-    {
-        return $this->lastName;
-    }
-
-    public function setLastName(string $lastName): self
-    {
-        $this->lastName = $lastName;
-
-        return $this;
-    }
 }
