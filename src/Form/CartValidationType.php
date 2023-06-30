ccd0d7f946320d2e260995518985bd06f8fdc4b4
WillyTALOM <103586241+WillyTALOM@users.noreply.github.com>
2023-06-30 18:46:34 +0200

    Update CartValidationType.php

--- src/Form/CartValidationType.php
+++ src/Form/CartValidationType.php
@@ -12,6 +12,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CartValidationType extends AbstractType
 {
